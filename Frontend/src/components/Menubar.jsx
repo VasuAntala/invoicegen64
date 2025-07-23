2420261c380455f86e1154751b8148c00347b6de
VasuAntala <vasuantala8283@gmail.com>
2025-07-23 17:08:36 +0530

    m

--- Frontend/src/components/Menubar.jsx
+++ Frontend/src/components/Menubar.jsx
@@ -69,36 +69,3 @@ const Manubar = () => {
 }
 
 export default Manubar;
-// <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//     <div className="container-fluid">
-//         <a className="navbar-brand" href="#">Invoice Generator</a>
-//         <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-//                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-//                 aria-expanded="false" aria-label="Toggle navigation">
-//             <span className="navbar-toggler-icon"></span>
-//         </button>
-        // <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        //     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        //         <li className="nav-item">
-        //             <a className="nav-link active" aria-current="page" href="/">Home</  a>
-        //         </li>
-        //         <li className="nav-item">       
-        //             <a className="nav-link" href="/mainpagee">Main Page</a>
-        //         </li>
-        //         <li className="nav-item">
-        //             <a className="nav-link" href="/dashboard">Dashboard</a>
-        //         </li>
-        //         <li className="nav-item">
-        //             <a className="nav-link" href="/previewpage">Preview Page</a>
-        //         </li>
-        //         <li className="nav-item">
-        //             <a className="nav-link" href="/landingpage">Landing Page</a>
-        //         </li>
-        //     </ul>
-//             <form className="d-flex" role="search">
-//                 <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-//                 <button className="btn btn-outline-success" type="submit">Search</button>
-//             </form>
-//         </div>
-//     </div>
-// </nav>      
