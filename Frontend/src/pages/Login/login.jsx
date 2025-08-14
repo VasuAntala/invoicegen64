e8d66ecea125bc5e2db2df1d0a3a75b494b1d6d3
VasuAntala <vasuantala8283@gmail.com>
2025-08-14 17:36:24 +0530

    login

--- Frontend/src/pages/Login/login.jsx
+++ Frontend/src/pages/Login/login.jsx
@@ -89,7 +89,7 @@ const Login = () => {
                             onChange={handleChange}
                             required
                         />
-                     
+                     <br />
 
                         <label htmlFor="email">Email:</label>
                         <input
@@ -102,7 +102,7 @@ const Login = () => {
                             onChange={handleChange}
                             required
                         />
-                       
+                       <br />
                         <label htmlFor="password">Password:</label>
                         <input
                             type="password"
