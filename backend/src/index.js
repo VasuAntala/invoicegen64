783bf7be44b433f17965087abb05324d92ee66af
VasuAntala <vasuantala8283@gmail.com>
2025-10-18 19:36:25 +0530

    render s

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,30 @@ import { mongooseConnection } from './database/connection/connection.js';
 const app = express();
 
 // CORS for React frontend
-app.use(cors({ origin:"https://vercel.com/vasu-antalas-projects/invoicegen64-aacq/C8kbneJuspUKRw1F4afkZFZpBMu9" }));
+// app.use(cors({ origin:"https://vercel.com/vasu-antalas-projects/invoicegen64-aacq/C8kbneJuspUKRw1F4afkZFZpBMu9" }));
+
+
+
+const allowedOrigins = [
+  "http://localhost:5173", // local development
+  "https://invoicegen64-aacq.vercel.app",
+  "https://invoicegen64-j6xn.vercel.app",
+  "https://invoicegen64-aacq-git-main-vasu-antalas-projects.vercel.app" // ✅ new vercel build URL
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.log("❌ Blocked by CORS:", origin);
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
